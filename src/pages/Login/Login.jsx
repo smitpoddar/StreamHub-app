b54cce987b84a9695866139b5a2a42ec43c2e409
Smit kumar <141864912+smitpoddar@users.noreply.github.com>
2025-11-14 00:33:09 +0530

    Update Login.jsx

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -37,7 +37,7 @@ const Login = () => {
           email,
           password
         );
-        navigate("/home");
+        navigate("/");
         setloading(false)
       } else {
         setloading(false);
